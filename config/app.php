db17fde830c01079974988dd76471171d749402b
Jaime Martínez <jaime.martinez@tecalis.com>
2021-04-20 09:16:54 +0200

    loggin and config

---
+++ config/app.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'name'                            => env('APP_NAME'),
+    'env'                             => env('APP_ENV'),
+    'debug'                           => env('APP_DEBUG'),
+    'debug_requests'                  => env('APP_DEBUG_REQUESTS'),
+    'base_url'                        => env('APP_URL'),
+    'timezone'                        => env('APP_TIMEZONE'),
+    'example_log_channel'             => env('EXAMPLE_LOG_CHANNEL', 'exam_log'),
+];
